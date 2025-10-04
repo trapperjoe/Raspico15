14086ac5281fa5fc0ce422ecc2e00b2d111c1870
Johannes Marko <147028901+trapperjoe@users.noreply.github.com>
2025-10-04 16:36:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import time
 
 pin = Pin(0,Pin.IN,Pin.PULL_UP)
 if pin.value() is 0:
-    ugit.pull_all()
+    # ugit.pull_all()
+    pass
     
 #main code here
 TIME_MS=100
 LED = Pin("LED", Pin.OUT)
 while True:
-    LED.off()
-    time.sleep_ms(TIME_MS)
     LED.on()
+    time.sleep_ms(TIME_MS)
+    LED.off()
     time.sleep_ms(TIME_MS)
